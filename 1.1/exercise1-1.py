b96ca3490e689aacb7e45f70b5f40c8791cbbd6b
Luis Echegaray <mrluisechegaray@gmail.com>
2014-12-21 18:33:45 -0500

    finished exercise 1.1

--- 1.1/exercise1-1.py
+++ 1.1/exercise1-1.py
@@ -1,3 +1,14 @@
 # You have N-element tuple or sequence that you would like
 # to unpack into a collection of N variables
 
+t = ("a", "b", "mpilgrim", "z", "example")
+# .split() wont work since tuples don't have attributes
+
+a, b, c, d, e = t
+# gotta match exactly or recieve "ValueError: too many values to unpack (expected 4)"
+
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
